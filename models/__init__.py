fda4ca327a6c5e90f45d3cde96826be0a032e289
jenntang1 <1039@holbertonschool.com>
2020-03-31 21:20:14 +0000

    Add =='db' to init method

--- models/__init__.py
+++ models/__init__.py
@@ -9,7 +9,8 @@ from models.place import Place
 from models.review import Review
 import os
 
-if os.getenv('HBNB_TYPE_STORAGE'):
+
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
