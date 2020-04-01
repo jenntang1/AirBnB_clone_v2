e4ad06fc93fcd248e6ef1f7c7f5349397532f73c
eNobreg <ezra.nobrega@outlook.com>
2020-04-01 17:10:52 -0400

    [Update] Changed files for task 10

--- models/place.py
+++ models/place.py
@@ -11,12 +11,12 @@ metadata = Base.metadata
 
 place_amenity = Table('place_amenity', metadata,
                       Column('place_id', String(60),
-                             primary_key=True,
                              ForeignKey('places.id'),
+                             primary_key=True,
                              nullable=False),
                       Column('amenity_id', String(60),
-                             primary_key=True,
                              ForeignKey('amenities.id'),
+                             primary_key=True,
                              nullable=False))
 
 
@@ -83,7 +83,7 @@ class Place(BaseModel, Base):
         longitude = Column(Float, nullable=True)
         amenity_ids = []
 
-        amenities = relationship("Amenity", secondary='place_amenity',
-                                 viewonly=False)
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False, backref="place_amenities")
         reviews = relationship("Review", cascade='all, delete',
                                backref='place')
