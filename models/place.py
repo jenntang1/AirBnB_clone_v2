a58599d8d99dde7879717168b0ad505e06da6a0c
jenntang1 <1039@holbertonschool.com>
2020-04-01 16:35:51 +0000

    [Update] Uncomment getter

--- models/place.py
+++ models/place.py
@@ -2,7 +2,7 @@
 """This is the place class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
-# import os
+import os
 from sqlalchemy.orm import relationship
 from models.review import Review
 
@@ -64,21 +64,21 @@ class Place(BaseModel, Base):
 #                    new_list.append(value)
 #            return new_list
 
-#    else:
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-    amenity_ids = []
+    else:
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, default=0, nullable=False)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
+        max_guest = Column(Integer, default=0, nullable=False)
+        price_by_night = Column(Integer, default=0, nullable=False)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        amenity_ids = []
 
 #        amenities = relationship("Amenity", cascade='all, delete',
 #                                 backref='state', secondary='place_amenity',
 #                                 viewonly=False)
-    reviews = relationship("Review", cascade='all, delete',
-                           backref='place')
+        reviews = relationship("Review", cascade='all, delete',
+                               backref='place')
