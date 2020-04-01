85c867162d933921765b1943efe91295a9a9acf3
jenntang1 <1039@holbertonschool.com>
2020-04-01 01:59:48 +0000

    [Update] Comment out codes

--- models/place.py
+++ models/place.py
@@ -6,17 +6,17 @@ import os
 from sqlalchemy.orm import relationship
 
 
-metadata = Base.metadata
+#metadata = Base.metadata
 
-place_amenity = Table('place_amenity', metadata,
-                      Column('place_id', String(60),
-                             ForeignKey('places.id'),
-                             primary_key=True,
-                             nullable=False),
-                      Column('amenity_id', String(60),
-                             ForeignKey('amenities.id'),
-                             primary_key=True,
-                             nullable=False))
+#place_amenity = Table('place_amenity', metadata,
+#                      Column('place_id', String(60),
+#                             ForeignKey('places.id'),
+#                             primary_key=True,
+#                             nullable=False),
+#                      Column('amenity_id', String(60),
+#                             ForeignKey('amenities.id'),
+#                             primary_key=True,
+#                             nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -36,49 +36,49 @@ class Place(BaseModel, Base):
     """
     __tablename__ = 'places'
 
-    if (os.getenv('HBNB_TYPE_STORAGE') != 'db'):
+#   if (os.getenv('HBNB_TYPE_STORAGE') != 'db'):
 
-        @property
-        def reviews(self):
-            """
-            Returns list of review instances
-            """
-            new_list = []
-            all_entries = models.storage.all()
-            for key, value in all_entries.items():
-                split_key = entry.split('.')
-                if split_key[1] == Place.id:
-                    new_list.append(value)
-            return new_list
+#        @property
+#        def reviews(self):
+#            """
+#            Returns list of review instances
+#            """
+#            new_list = []
+#            all_entries = models.storage.all()
+#            for key, value in all_entries.items():
+#                split_key = entry.split('.')
+#                if split_key[1] == Place.id:
+#                    new_list.append(value)
+#            return new_list
 
-        @property
-        def amenities(self):
-            """
-            Returns list of amenity instances
-            """
-            new_list = []
-            all_entries = models.storage.all()
-            for key, value in all_entries.items():
-                split_key = entry.split('.')
-                if split_key[1] == Place.id:
-                    new_list.append(value)
-            return new_list
+#        @property
+#        def amenities(self):
+#            """
+#            Returns list of amenity instances
+#            """
+#            new_list = []
+#            all_entries = models.storage.all()
+#            for key, value in all_entries.items():
+#                split_key = entry.split('.')
+#                if split_key[1] == Place.id:
+#                    new_list.append(value)
+#            return new_list
 
-    else:
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, default=0, nullable=False)
-        number_bathrooms = Column(Integer, default=0, nullable=False)
-        max_guest = Column(Integer, default=0, nullable=False)
-        price_by_night = Column(Integer, default=0, nullable=False)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
-        amenity_ids = []
+#    else:
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    amenity_ids = []
 
-        amenities = relationship("Amenity", cascade='all, delete',
-                                 backref='state', secondary='place_amenity',
-                                 viewonly=False)
-        reviews = relationship("Review", cascade='all, delete',
-                               backref='place')
+#        amenities = relationship("Amenity", cascade='all, delete',
+#                                 backref='state', secondary='place_amenity',
+#                                 viewonly=False)
+#        reviews = relationship("Review", cascade='all, delete',
+#                               backref='place')
