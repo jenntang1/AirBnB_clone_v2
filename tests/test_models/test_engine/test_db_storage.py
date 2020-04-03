6df3491c8b76b543217e9791a1238ce87c917729
eNobreg <ezra.nobrega@outlook.com>
2020-04-02 21:20:17 -0400

    [Fix] Removed all commented out code from test_db

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -18,23 +18,5 @@ class TestDBStorage(unittest.TestCase):
         self.assertEqual(p.total_errors, 0, "Found code \
                          style errors (and warnings).")
 
-'''    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
-                     "Incorrect storage type")
-    def setUp(self):
-        """connect to test database"""
-        self.engine = create_engine('mysql+mysqldb://root: \
-                                    hbnb_test@localhost/hbnb_test_db',
-                                    pool_pre_ping=True)
-        self.session = Session(engine)
-        Base.metadata.create_all(self.engine)
-        self.session.add(self.user)
-        self.session.commit()
-
-    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
-                     "Incorrect storage type")
-    def tearDown(self):
-        Base.metadata.drop_all(self.engine)
-'''
-
 if __name__ == "__main__":
     unittest.main()
