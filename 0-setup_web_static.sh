aed1bc1c4748c4108676bee74a349826d21b2ec8
jenntang1 <1039@holbertonschool.com>
2020-04-06 19:06:00 +0000

    3. Updating variable for Nginx config

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,11 +12,11 @@ sudo mkdir -p /data/web_static/releases/test/
 
 sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 
-sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu: /data/
 
-var="location /hbnb_static/ {\n root /data/web_static/current/;\n}\n"
-sudo sed -i "55i$var" /etc/nginx/sites-available/default
+var="location /hbnb_static/ {\n alias /data/web_static/current/hbnb_static/;\n}\n"
+sudo sed -i "43i$var" /etc/nginx/sites-available/default
 
 sudo service nginx restart
