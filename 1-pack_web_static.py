0dd380ffecc01e82cd2de8ab997f514a389d8cfb
jenntang1 <1039@holbertonschool.com>
2020-04-06 22:46:36 +0000

    1. Creating fabric script to generate tgz archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" This Fabric script generates a .tgz archive """
+
+
+from fabric import *
+from fabric.contrib import files
+
+
+path = "tar -cvzf versions/web_static_$(date +%Y%m%d%H%M%S).tgz web_static"
+
+
+def do_pack():
+    """ Compress web static files into .tgz
+    Return:
+        upon success, returns archive path
+        upon fail, returns None
+    """
+    archive = sudo("mkdir -p versions; tar -cvzf \
+                   versions/web_static_$(date \
+                   +%Y%m%d%H%M%S).tgz web_static")
+
+    if archive.succeeded:
+        return path
+    return None
