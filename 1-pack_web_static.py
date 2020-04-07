37a2e3f3ee8aaccbfedfc9842db6190bf5f31f96
jenntang1 <1039@holbertonschool.com>
2020-04-07 02:14:42 +0000

    5. Updating to simplify getsize

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,9 +2,9 @@
 """ This Fabric script generates a .tgz archive """
 
 
-from fabric.api import *
+from fabric.api import run, local
 from fabric.contrib import files
-from os import stat
+from os.path import getsize
 
 
 def do_pack():
@@ -14,16 +14,14 @@ def do_pack():
         upon fail, returns None
     """
     timestamp = "+%Y%m%d%H%M%S"
-    tgzname = "web_static_{}.tgz".format(timestamp)
+    path = "versions/web_static_{}.tgz".format(timestamp)
 
-    versions = run("mkdir -p versions")
+    folder = run("mkdir -p versions")
 
-    archive = local("tar -cvzf versions/{} web_static".format(path))
+    archive = local("tar -cvzf {} web_static".format(path))
 
     if archive.succeeded:
-        size = path.getsize(tgzname)
-        print("web_static packed: versions/{} -> {}Bytes".format(
-                                                                 tgzname,
-                                                                 size))
-        return tgzname
+        size = getsize(path)
+        print("web_static packed: {} -> {}Bytes".format(path, size))
+        return path
     return None
