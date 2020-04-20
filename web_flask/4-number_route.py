99cc89fb6818423300e4d56bde388f85ef855049
jenntang1 <1039@holbertonschool.com>
2020-04-20 21:27:17 +0000

    1. Creating Python script using Flask to route url ending with an integer

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+""" Starts a Flask Web Application """
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ Method returns a hello message after
+    web app starts listening on 0.0.0.0:5000
+    Return:
+        “Hello HBNB!”
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Method returns a custom message after
+    web app starts listening on 0.0.0.0:5000
+    Return:
+        “HBNB”
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def show_c(text):
+    """ Method returns a custom text after
+    web app starts listening on 0.0.0.0:5000
+    Arg:
+        text to append to url
+    Return:
+        “C ” followed by the value of the
+        text variable (replace _ with space)
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python/", defaults={"text": "is_cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def show_python(text):
+    """ Method returns a custom text after
+    web app starts listening on 0.0.0.0:5000
+    Arg:
+        default text is "is cool"
+    Return:
+        “Python ” followed by the value of the
+        text variable (replace _ with space)
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def show_number(n):
+    """ Method returns a custom text after
+    web app starts listening on 0.0.0.0:5000
+    Arg:
+        number to append to url
+    Return:
+        “n is a number" only if n is an integer
+    """
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
