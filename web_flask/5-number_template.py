060545c7a71a497898c0138a4a5629f63fbcc258
jenntang1 <1039@holbertonschool.com>
2020-04-21 01:37:56 +0000

    1. Creating Python script using Flask to display HTML page

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+""" Starts a Flask Web Application """
+
+
+from os import path
+from flask import Flask, render_template
+
+
+project_root = path.dirname(path.dirname(path.join(__file__)))
+template_path = path.join(project_root, "templates/")
+app = Flask(__name__, template_folder=template_path)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ Method returns a hello message after
+    web app starts listening on 0.0.0.0:5000
+    Return:
+        “Hello HBNB!”
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Method returns a custom message after
+    web app starts listening on 0.0.0.0:5000
+    Return:
+        “HBNB”
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def show_c(text):
+    """ Method returns a custom text after
+    web app starts listening on 0.0.0.0:5000
+    Arg:
+        text to append to url
+    Return:
+        “C ” followed by the value of the
+        text variable (replace _ with space)
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python/", defaults={"text": "is_cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def show_python(text):
+    """ Method returns a custom text after
+    web app starts listening on 0.0.0.0:5000
+    Arg:
+        default text is "is cool"
+    Return:
+        “Python ” followed by the value of the
+        text variable (replace _ with space)
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def show_number(n):
+    """ Method returns a custom text after
+    web app starts listening on 0.0.0.0:5000
+    Arg:
+        number to append to url
+    Return:
+        “n is a number" only if n is an integer
+    """
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def html_template(n):
+    """ Method returns a HTML page after
+    web app starts listening on 0.0.0.0:5000
+    Arg:
+        number to append to url and display on body
+    Return:
+        an HTML page
+    """
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
