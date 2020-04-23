00b62b02ee199cb517bf701c4d221d65f90b5d65
jenntang1 <1039@holbertonschool.com>
2020-04-23 00:36:35 +0000

    3. Removing self from def close_db method

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,29 +8,30 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-def get_data():
-    """Method fetches data from storage
+def get_data(self):
+    """ Method fetches data from storage
     """
     data = storage.all(State)
 
 
 @app.route("/states_list", strict_slashes=False)
-def html_states(data):
+def html_states(self):
     """ Method returns a HTML page after
     web app starts listening on 0.0.0.0:5000
     Return:
         an HTML page that States and their ids
     """
+    data = get_data()
     return render_template("7-states_list.html", data=data)
 
 
 @app.teardown_appcontext
 def close_db(self):
-    """Method closes the database
+    """ Method closes the database
     Arg:
         storage data
     """
-    storage.close(self)
+    storage.close()
 
 
 if __name__ == "__main__":
