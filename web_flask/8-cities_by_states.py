d2aa853cd3ce9a4ebabf1a7006d36ed2f172469a
jenntang1 <1039@holbertonschool.com>
2020-04-24 21:26:49 +0000

    3. Updating cities data

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -28,9 +28,8 @@ def html_cities():
         an HTML page that displays the cities data
     """
     data = storage.all(State)
-    for states in data.values():
-        cities = state.cities
-    return render_template("8-cities_by_states.html", cities=cities)
+    states = data.values()
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
